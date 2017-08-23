6071959c8cbaa965b10aac8fec30453d9570c108
SaulYang <yangxinpenhua@qq.com>
2017-08-23 15:51:46 +0800

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,51 @@
+var Questions = { "question" : [
+  {
+    "question"  : "20期里没有谁？",
+     "choice1"  : "李金鹏",
+     "choice2"  : "faker",
+     "choice3"  : "李永辉",
+     "choice4"  : "李飞雄",
+     "choice5"  : "李大头",
+     "correct"  : 1
+  },
+  
+  {
+    "question"  : "21期里有谁？",
+     "choice1"  : "李永辉",
+     "choice2"  : "李飞雄",
+     "choice3"  : "李飞头",
+     "choice4"  : "李金鹏",
+     "choice5"  : "里小偷",
+     "correct"  : 4
+  },
+  
+  {
+    "question"  : "17期里还有谁？",
+     "choice1"  : "李金鹏",
+     "choice2"  : "潘红丽",
+     "choice3"  : "梨园",
+     "choice4"  : "王艳杰",
+     "choice5"  : "杨欣",
+     "correct"  : 2
+  },
+  
+  {
+    "question"  : "乐美现在有多少期",
+     "choice1"  : "20",
+     "choice2"  : "19",
+     "choice3"  : "22",
+     "choice4"  : "23",
+     "choice5"  : "21",
+     "correct"  : 5
+  },
+  
+  {
+    "question"  : "乐美培训谁的年龄最大？",
+     "choice1"  : "杨欣",
+     "choice2"  : "李金鹏",
+     "choice3"  : "窦连军",
+     "choice4"  : "无敌",
+     "choice5"  : "外星人",
+     "correct"  : 1
+  }
+]};
